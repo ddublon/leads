fba76fc2832a08a5a7d40a2d207f98c0527764a4
Dolev Dublon <127298010+ddublon@users.noreply.github.com>
2024-08-06 15:39:40 +0300

    medtronics (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,16 @@ const solidFillBody = new LinearGradientFill({
   ],
 });
 
+const segmentedWhiteBar = new SolidFill({
+  // to optimize this I can share the same BackgroundFill object across all charts
+  color: ColorHEX("#eeeeee"),
+});
+
+const segmentedWhiteDarkerBar = new SolidFill({
+  // to optimize this I can share the same BackgroundFill object across all charts
+  color: ColorHEX("#A3A3A3"),
+});
+
 const solidFillBackgroundCircle = new LinearGradientFill({
   angle: 90,
   stops: [
@@ -90,11 +100,136 @@ const drawText = (
   updatePosition(contactLabelTextBox);
 };
 
-const drawLeadBody = (polygonSeries: PolygonSeries, topBody: number) => {
+const drawClosedSegmented = (
+  top: number,
+  text: string,
+  polygonSeries: PolygonSeries,
+  chart: ChartXY,
+  axisX: Axis,
+  axisY: Axis,
+  ellipseSeries: EllipseSeries,
+  polygonSeriesSegmetedWhiteLines: PolygonSeries
+) => {
+  // Constants for left and right positions
+  const left = 4.35;
+  const right = 5.65;
+  const upperCircle = ellipseSeries
+    .add({ x: 5, y: top, radiusX: 0.65, radiusY: 0.2 })
+    // Optional, changing default style
+    .setFillStyle(solidFillBackgroundCircle)
+    .setStrokeStyle(
+      new SolidLine({ thickness: 1, fillStyle: solidFillBackgroundCircle })
+    );
+
+  // Calculate bottom from top by adding 1.5
+  const bottom = top - 1.5;
+
+  const bottomCircle = ellipseSeries
+    .add({ x: 5, y: bottom, radiusX: 0.65, radiusY: 0.2 })
+    // Optional, changing default style
+    .setFillStyle(solidFillContact)
+    .setStrokeStyle(
+      new SolidLine({ thickness: 1, fillStyle: solidFillContact })
+    );
+
+  // Calculate center coordinates
+  const centerX = (left + right) / 2;
+  const centerY = (top + bottom) / 2;
+  const center = { x: centerX, y: centerY };
+  // Draw polygon representing contact
+  polygonSeries
+    .add([
+      { x: centerX, y: top },
+      { x: right, y: top },
+      { x: right, y: bottom },
+      { x: left, y: bottom },
+      { x: left, y: top },
+    ])
+    .setFillStyle(solidFillContact)
+    .setStrokeStyle(
+      new SolidLine({
+        thickness: 1,
+        fillStyle: solidFillContact,
+      })
+    );
+
+  // draw white lines for segmented contact
+  polygonSeriesSegmetedWhiteLines
+    .add([
+      { x: centerX - 0.03, y: top + 0.2 },
+      { x: centerX + 0.03, y: top + 0.2 },
+      { x: centerX + 0.03, y: top - 0.2 },
+      { x: centerX - 0.03, y: top - 0.2 },
+      { x: centerX - 0.03, y: top + 0.2 },
+    ])
+    .setFillStyle(segmentedWhiteBar)
+    .setStrokeStyle(
+      new SolidLine({
+        thickness: 1,
+        fillStyle: segmentedWhiteBar,
+      })
+    );
+
+  polygonSeriesSegmetedWhiteLines
+    .add([
+      { x: right - 0.03 - 0.2, y: top - 0.15 },
+      { x: right + 0.03 - 0.2, y: top - 0.14 },
+      { x: right + 0.03 - 0.2, y: bottom - 0.14 },
+      { x: right - 0.03 - 0.2, y: bottom - 0.15 },
+      { x: right - 0.03 - 0.2, y: top - 0.2 },
+    ])
+    .setFillStyle(segmentedWhiteDarkerBar)
+    .setStrokeStyle(
+      new SolidLine({
+        thickness: 1,
+        fillStyle: segmentedWhiteDarkerBar,
+      })
+    );
+
+  polygonSeriesSegmetedWhiteLines
+    .add([
+      { x: right - 0.03 - 0.2, y: top - 0.15 },
+      { x: right + 0.03 - 0.2, y: top - 0.14 },
+      { x: right + 0.03 - 0.2, y: bottom - 0.14 },
+      { x: right - 0.03 - 0.2, y: bottom - 0.15 },
+      { x: right - 0.03 - 0.2, y: top - 0.2 },
+    ])
+    .setFillStyle(segmentedWhiteDarkerBar)
+    .setStrokeStyle(
+      new SolidLine({
+        thickness: 1,
+        fillStyle: segmentedWhiteDarkerBar,
+      })
+    );
+
+  polygonSeriesSegmetedWhiteLines
+    .add([
+      { x: left + 0.03 + 0.2, y: top - 0.15 },
+      { x: left - 0.03 + 0.2, y: top - 0.14 },
+      { x: left - 0.03 + 0.2, y: bottom - 0.14 },
+      { x: left + 0.03 + 0.2, y: bottom - 0.15 },
+      { x: left + 0.03 + 0.2, y: top - 0.2 },
+    ])
+    .setFillStyle(segmentedWhiteDarkerBar)
+    .setStrokeStyle(
+      new SolidLine({
+        thickness: 1,
+        fillStyle: segmentedWhiteDarkerBar,
+      })
+    );
+
+  drawText(chart, axisX, axisY, text, center);
+};
+
+const drawLeadBody = (
+  polygonSeries: PolygonSeries,
+  topBody: number,
+  bottomBody: number,
+  ellipseSeries: EllipseSeries
+) => {
   const left = 4.35;
   const right = 5.65;
 
-  const bottomBody = 3;
   polygonSeries
     .add([
       { x: left, y: topBody },
@@ -110,6 +245,17 @@ const drawLeadBody = (polygonSeries: PolygonSeries, topBody: number) => {
         fillStyle: solidFillBody,
       })
     );
+
+
+    ellipseSeries
+    .add({ x: 5, y: bottomBody, radiusX: 0.65, radiusY: 0.7 })
+    // Optional, changing default style
+    .setFillStyle(solidFillBody)
+    .setStrokeStyle(
+      new SolidLine({ thickness: 1, fillStyle: solidFillBody })
+    );
+
+
 };
 
 const drawContact = (
@@ -143,6 +289,7 @@ const drawContact = (
     .setStrokeStyle(
       new SolidLine({ thickness: 1, fillStyle: solidFillContact })
     );
+
   // Calculate center coordinates
   const centerX = (left + right) / 2;
   const centerY = (top + bottom) / 2;
@@ -249,12 +396,12 @@ const drawLeftContact = (
   // Calculate bottom from top by adding 1.5
   const bottom = top - 1.5;
   const bottomCircle = ellipseSeries
-  .add({ x: midpoint, y: bottom, radiusX: 0.65, radiusY: 0.2 })
-  // Optional, changing default style
-  .setFillStyle(solidFillContact)
-  .setStrokeStyle(
-    new SolidLine({ thickness: 1, fillStyle: solidFillContact })
-  );
+    .add({ x: midpoint, y: bottom, radiusX: 0.65, radiusY: 0.2 })
+    // Optional, changing default style
+    .setFillStyle(solidFillContact)
+    .setStrokeStyle(
+      new SolidLine({ thickness: 1, fillStyle: solidFillContact })
+    );
   // Calculate center coordinates
   const centerX = (left + right) / 2;
   const centerY = (top + bottom) / 2;
@@ -350,9 +497,11 @@ const App = () => {
         .setBackgroundFillStyle(BackgroundFill)
         .setSeriesBackgroundFillStyle(BackgroundFill);
       let polygonSeries: PolygonSeries = chart.polygonSeriesLead;
+      let polygonSeriesSegmetedWhiteLines: PolygonSeries =
+        chart.polygonSeriesLeadSegmetedWhiteLines;
       let ellipseSeries: EllipseSeries = chart.ellipseSeriesLead;
 
-      const electrodeName = "Boston Scientific Vercise";
+      const electrodeName = "Medtronic 33005";
       if (electrodeName === "Boston Scientific Vercise") {
         // Boston Scientific Vercise Segmented
         // decided to make 5 as the center of the lead.
@@ -360,7 +509,11 @@ const App = () => {
         const axisY = chart.getDefaultAxisY();
 
         // Initialize polygonSeries if it doesn't exist
-        if (!polygonSeries && !ellipseSeries) {
+        if (
+          !polygonSeries &&
+          !ellipseSeries &&
+          !polygonSeriesSegmetedWhiteLines
+        ) {
           polygonSeries = chart.addPolygonSeries({ axisX, axisY });
           polygonSeries.setAutoScrollingEnabled(false);
           console.log({ polygonSeries });
@@ -368,6 +521,18 @@ const App = () => {
           polygonSeries.setEffect(false);
           polygonSeries.setAutoScrollingEnabled(false);
           polygonSeries.setHighlightOnHover(false);
+
+          polygonSeriesSegmetedWhiteLines = chart
+            .addPolygonSeries({ axisX, axisY })
+            .setDrawOrder({ seriesDrawOrderIndex: 10 });
+          polygonSeriesSegmetedWhiteLines.setAutoScrollingEnabled(false);
+          console.log({ polygonSeriesSegmetedWhiteLines });
+          chart.polygonSeriesSegmetedWhiteLines =
+            polygonSeriesSegmetedWhiteLines;
+          polygonSeriesSegmetedWhiteLines.setEffect(false);
+          polygonSeriesSegmetedWhiteLines.setAutoScrollingEnabled(false);
+          polygonSeriesSegmetedWhiteLines.setHighlightOnHover(false);
+
           chart.setAutoCursorMode(AutoCursorModes.disabled);
           ellipseSeries = chart
             .addEllipseSeries()
@@ -377,7 +542,7 @@ const App = () => {
           chart.ellipseSeriesLead = ellipseSeries;
         }
 
-        drawLeadBody(polygonSeries, 10);
+        // drawLeadBody(polygonSeries, 10);
         drawContact(9, "4", polygonSeries, chart, axisX, axisY, ellipseSeries);
         drawContact(7, "3A", polygonSeries, chart, axisX, axisY, ellipseSeries);
         drawRightContact(
@@ -426,6 +591,115 @@ const App = () => {
           axisY,
           ellipseSeries
         );
+      } else if (electrodeName === "Medtronic 33005") {
+        // Boston Scientific Vercise Segmented
+        // decided to make 5 as the center of the lead.
+        const axisX = chart.getDefaultAxisX();
+        const axisY = chart.getDefaultAxisY();
+
+        // Initialize polygonSeries if it doesn't exist
+        if (!polygonSeries && !ellipseSeries) {
+          polygonSeries = chart.addPolygonSeries({ axisX, axisY });
+          polygonSeries.setAutoScrollingEnabled(false);
+          console.log({ polygonSeries });
+          chart.polygonSeriesLead = polygonSeries;
+          polygonSeries.setEffect(false);
+          polygonSeries.setAutoScrollingEnabled(false);
+          polygonSeries.setHighlightOnHover(false);
+          chart.setAutoCursorMode(AutoCursorModes.disabled);
+
+          polygonSeriesSegmetedWhiteLines = chart
+            .addPolygonSeries({ axisX, axisY })
+            .setDrawOrder({ seriesDrawOrderIndex: 10 });
+          polygonSeriesSegmetedWhiteLines.setAutoScrollingEnabled(false);
+          console.log({ polygonSeriesSegmetedWhiteLines });
+          chart.polygonSeriesSegmetedWhiteLines =
+            polygonSeriesSegmetedWhiteLines;
+          polygonSeriesSegmetedWhiteLines.setEffect(false);
+          polygonSeriesSegmetedWhiteLines.setAutoScrollingEnabled(false);
+          polygonSeriesSegmetedWhiteLines.setHighlightOnHover(false);
+
+          ellipseSeries = chart
+            .addEllipseSeries()
+            .setAutoScrollingEnabled(false)
+            .setHighlightOnHover(false)
+            .setEffect(false);
+          chart.ellipseSeriesLead = ellipseSeries;
+        }
+
+        drawLeadBody(polygonSeries, 10, 1, ellipseSeries);
+        drawContact(9, "3", polygonSeries, chart, axisX, axisY, ellipseSeries);
+        drawClosedSegmented(
+          7,
+          "2",
+          polygonSeries,
+          chart,
+          axisX,
+          axisY,
+          ellipseSeries,
+          polygonSeriesSegmetedWhiteLines
+        );
+
+        drawClosedSegmented(
+          5,
+          "1",
+          polygonSeries,
+          chart,
+          axisX,
+          axisY,
+          ellipseSeries,
+          polygonSeriesSegmetedWhiteLines
+        );
+
+        drawContact(3, "0", polygonSeries, chart, axisX, axisY, ellipseSeries);
+
+        // drawContact(7, "3A", polygonSeries, chart, axisX, axisY, ellipseSeries);
+        // drawRightContact(
+        //   7,
+        //   "3B",
+        //   polygonSeries,
+        //   chart,
+        //   axisX,
+        //   axisY,
+        //   ellipseSeries
+        // );
+        // drawLeftContact(
+        //   7,
+        //   "3C",
+        //   polygonSeries,
+        //   chart,
+        //   axisX,
+        //   axisY,
+        //   ellipseSeries
+        // );
+        // drawContact(5, "2A", polygonSeries, chart, axisX, axisY, ellipseSeries);
+        // drawRightContact(
+        //   5,
+        //   "2B",
+        //   polygonSeries,
+        //   chart,
+        //   axisX,
+        //   axisY,
+        //   ellipseSeries
+        // );
+        // drawLeftContact(
+        //   5,
+        //   "2B",
+        //   polygonSeries,
+        //   chart,
+        //   axisX,
+        //   axisY,
+        //   ellipseSeries
+        // );
+        // drawLastContact(
+        //   3,
+        //   "1",
+        //   polygonSeries,
+        //   chart,
+        //   axisX,
+        //   axisY,
+        //   ellipseSeries
+        // );
       }
 
       return () => {
@@ -441,7 +715,12 @@ const App = () => {
   return (
     <div
       id="chart"
-      style={{ width: "500px", height: "500px", border: "1px black solid" }}
+      style={{
+        width: "500px",
+        height: "500px",
+        border: "1px black solid",
+        backgroundColor: "black",
+      }}
     ></div>
   );
 };
